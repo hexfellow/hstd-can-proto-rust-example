5a46d3ff32b5076b34afd055250e5b3fede2c12e
KisonHe <kisonmaxine@gmail.com>
2024-11-02 14:58:38 +0800

    Apply new pb

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<()> {
         &[
             "src/hstd-can-proto/hexstd_can_msg.proto",
             "src/hstd-can-proto/hex_bridge.proto",
-            "src/hstd-can-proto/hex_bridge_types.proto",
+            "src/hstd-can-proto/hex_bridge_control.proto",
         ],
         &["src/hstd-can-proto"],
     )?;
